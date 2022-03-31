28bc1dac52d9d324f425ddebb413eb09379436c9
Kouki Sawada <github@sawada.pro>
2022-03-31 22:13:41 +0900

    typesから型定義を移植、isPageQueryの動作を厳格化

--- lib/typeGuards/isPageQuery.ts
+++ lib/typeGuards/isPageQuery.ts
@@ -1,4 +1,11 @@
-import { pageQuery } from '~/types/pagination';
+type pageQuery = {
+    page: string;
+};
 
-export const isPageQuery = (v: any): v is pageQuery =>
-    typeof v.page === 'string';
+export const isPageQuery = (v: any): v is pageQuery => {
+    if (typeof v.page === 'string') {
+        const n = Number(v.page);
+        return !isNaN(n);
+    }
+    return false;
+};
