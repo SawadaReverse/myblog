d941ffc64df2621be0b5efb24695464d4446b77d
SawadaReverse <github@sawada.pro>
2022-04-25 23:55:09 +0900

    Revert "とりあえずタイムゾーン見せてくれよ"
    This reverts commit ef8c968e9d1b51ec2c1d296fcc61ff003ed0a24d.

--- components/articleCard.vue
+++ components/articleCard.vue
@@ -39,7 +39,7 @@ export default defineComponent({
         const { $dayjs } = context;
         const edited = props.article;
         edited.createdAt = $dayjs(edited.createdAt).format(
-            'YYYY/MM/DD HH:mm:ss Z'
+            'YYYY/MM/DD HH:mm:ss'
         );
 
         return {
