2ba810dc7085c951c639e37aa1dd35e2705ef6cf
SawadaReverse <github@sawada.pro>
2022-08-15 21:11:30 +0900

    軽微な修正

--- components/surroundPagination.vue
+++ components/surroundPagination.vue
@@ -4,9 +4,9 @@
             前後の記事を取得できませんでした。
         </v-container>
 
-        <v-container>
-            <v-row v-if="!isErr">
-                <div v-if="surroundArticles[0].path !== ''">
+        <v-container v-if="!isErr">
+            <v-row>
+                <div v-if="surroundArticles[0].path">
                     ≪
                     <nuxt-link :to="surroundArticles[0].path">
                         前の記事へ
