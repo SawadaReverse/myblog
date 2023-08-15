664882b8d091ec97a39d396a5f524ab11b34a725
SawadaReverse <github@sawada.pro>
2023-08-16 01:49:32 +0900

    型追加

--- src/app/api/types/types.d.ts
+++ src/app/api/types/types.d.ts
@@ -1,4 +1,11 @@
+import { Article, Tag } from '@/libs/microCms/types';
+import { MicroCMSListResponse } from 'microcms-js-sdk';
+
 export type ApiResponse<T> = {
   result?: T;
   message: string;
+};
+
+export type TagDetail = Tag & {
+  articles: MicroCMSListResponse<Article>;
 };
