cfc433a36dbf5c3d789e9d5c3a387bb1ee9f6d53
SawadaReverse <github@sawada.pro>
2023-08-16 01:47:54 +0900

    ページネーション仕様変更に伴う変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
-"use client";
+'use client';
 
-import { Box, CircularProgress, Divider } from "@mui/material";
-import ArticleDescription from "@/components/ArticleDescription";
-import Paging from "@/components/Paging";
-import useSWR from "swr";
-import { ApiResponse } from "./api/types/types";
-import { Article } from "@/libs/microCms/types";
-import { fetcher } from "@/libs/swr/fetcher";
-import { MicroCMSListResponse } from "microcms-js-sdk";
+import { Box, CircularProgress, Divider } from '@mui/material';
+import ArticleDescription from '@/components/ArticleDescription';
+import Paging from '@/components/Paging';
+import useSWR from 'swr';
+import { ApiResponse } from './api/types/types';
+import { Article } from '@/libs/microCms/types';
+import { fetcher } from '@/libs/swr/fetcher';
+import { MicroCMSListResponse } from 'microcms-js-sdk';
 
 type Props = {
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
 export default function Home(props: Props) {
-  const pageParam = props.searchParams["page"];
+  const pageParam = props.searchParams['page'];
   const page =
     pageParam && !Array.isArray(pageParam) && !Number.isNaN(parseInt(pageParam))
       ? parseInt(pageParam)
@@ -27,15 +27,15 @@ export default function Home(props: Props) {
   if (isLoading || !data || !data.result)
     return (
       <>
-        <Box sx={{ textAlign: "center" }}>
+        <Box sx={{ textAlign: 'center' }}>
           <CircularProgress />
         </Box>
       </>
     );
-  if (!!error) {
+  if (error) {
     throw new Error(error.message);
   }
-  if (!!data.message) {
+  if (data.message) {
     throw new Error(data.message);
   }
   return (
@@ -47,7 +47,7 @@ export default function Home(props: Props) {
         </Box>
       ))}
 
-      <Paging totalCount={data.result.totalCount} currentPage={page} />
+      <Paging totalCount={data.result.totalCount} />
     </>
   );
 }
--- src/components/Paging.tsx
+++ src/components/Paging.tsx
@@ -1,23 +1,33 @@
-"use client";
+'use client';
 
-import { Box, Pagination } from "@mui/material";
-import { useRouter } from "next/navigation";
+import { Box, Pagination } from '@mui/material';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 type Props = {
   totalCount: number;
-  currentPage: number;
 };
 
 export default function Paging(props: Props) {
+  const path = usePathname();
   const router = useRouter();
   const onChangePagination = (_: React.ChangeEvent<unknown>, value: number) =>
-    value === 1 ? router.push("/") : router.push(`/?page=${value}`);
+    value === 1 ? router.push(path) : router.push(`${path}?page=${value}`);
+
+  const param = useSearchParams();
+  const pageParam = param.get('page');
+  let page: number;
+  if (!pageParam || isNaN(parseInt(pageParam))) {
+    page = 1;
+  } else {
+    page = parseInt(pageParam);
+  }
+
   return (
     <>
-      <Box sx={{ justifyContent: "center", display: "flex" }}>
+      <Box sx={{ justifyContent: 'center', display: 'flex' }}>
         <Pagination
-          count={props.totalCount / 10}
-          page={props.currentPage}
+          count={Math.ceil(props.totalCount / 10)}
+          page={page}
           onChange={onChangePagination}
         />
       </Box>
