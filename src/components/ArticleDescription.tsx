443ec89af2256eb8bdd6cb699ad873c2754610bc
SawadaReverse <github@sawada.pro>
2023-09-24 03:50:36 +0900

    dayjsの挙動修正をしてみる

--- src/components/ArticleDescription.tsx
+++ src/components/ArticleDescription.tsx
@@ -4,13 +4,19 @@ import { Box, Typography, Link } from '@mui/material';
 import TagArray from './TagArray';
 import { Article } from '@/app/api/types/types';
 import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+import ja from 'dayjs/locale/ja';
+
+dayjs.locale(ja);
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 type Props = {
   article: Article;
 };
 
 export default function ArticleDescription(props: Props) {
-  dayjs.locale('ja');
   return (
     <>
       <Box>
@@ -21,8 +27,9 @@ export default function ArticleDescription(props: Props) {
         >
           <Typography variant="h5">{props.article.title}</Typography>
           <Typography variant="body2" color="text.secondary" component="div">
-            {dayjs(props.article.publishedAt).format('YYYY/MM/DD HH:mm:ss') ??
-              props.article.publishedAt}
+            {dayjs(props.article.publishedAt)
+              .tz('Asia/Tokyo')
+              .format('YYYY/MM/DD HH:mm:ss') ?? props.article.publishedAt}
           </Typography>
           <Typography variant="subtitle1" color="text.secondary" sx={{ my: 2 }}>
             {props.article.description}
